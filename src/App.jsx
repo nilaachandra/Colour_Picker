80fd389fd9aac537ec884bf62ca4dbb3ebc08f54
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-02-07 22:44:53 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ColorPicker from "./components/ColorPicker";
 function App() {
   return (
     <>
-      <div className="main p-4 m-0">
+      <div className="main p-4 m-0 flex justify-center items-center">
         <ColorPicker />
       </div>
     </>
