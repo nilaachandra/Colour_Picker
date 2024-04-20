c2e8c88516d290b9f289948e3d269e8c1ed14be8
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-04-21 00:12:53 +0530

    Gradient Picker Added

--- README.md
+++ README.md
@@ -1,8 +1,56 @@
-# React + Vite
+# ColorsByNilaa
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Welcome to ColorsByNilaa, a simple web application for creating beautiful gradients and color combinations!
 
-Currently, two official plugins are available:
+## Live Link
+https://colorsbynilaa.vercel.app
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Overview
+
+ColorsByNilaa allows you to effortlessly generate custom color gradients and experiment with different color combinations. Whether you're designing a website, creating artwork, or just exploring your creativity, Colors By Nilaa provides a user-friendly interface to help you find the perfect color scheme.
+
+## Features
+
+- **Gradient Picker**: Choose between linear and radial gradients and adjust the angle, colors, and position/percentage to create stunning gradient effects.
+- **Color Picker**: Select colors using an intuitive color picker tool, allowing you to fine-tune the hues, saturation, and brightness.
+- **Copy to Clipboard**: Easily copy the generated CSS code for your gradient or color selection with the click of a button.
+- **Simple Interface**: Clean and minimalist design ensures a pleasant user experience, making it easy to focus on creating your desired color schemes.
+
+## Getting Started
+
+To get started with Colors By Nilaa, follow these steps:
+
+1. Clone the repository to your local machine:
+```
+git clone https://github.com/nilaachandra/Colour_Picker
+```
+2. Install the project dependencies using npm:
+```
+npm install
+```
+3. Running the Project
+```
+npm run dev
+```
+4. Open http://localhost:5173 in your browser to view the project.
+
+## Technologies Used
+
+Colors By Nilaa is built using the following technologies:
+
+- React: A JavaScript library for building user interfaces.
+- Vite: A fast, modern build tool that significantly improves the frontend development experience. 
+- Tailwind CSS: A utility-first CSS framework for rapidly building custom designs.
+- Sonner: A minimal and easy to use toast library.
+
+
+## How to Contribute
+
+If you'd like to contribute to ColorsByNilaa, feel free to fork the repository and submit a pull request with your proposed changes. Whether it's adding new features, fixing bugs, or improving the user interface, all contributions are welcome!
+
+
+
+## Acknowledgements
+- I would love to thank the Web Development Community on Twitter for encouraging me to build this small project.
+- I would love to thank Youtube for the resources they provided
+
--- src/components/GadientPicker.jsx
+++ src/components/GadientPicker.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Button from './Button';
+import { Toaster, toast } from 'sonner'
+import { Link } from 'react-router-dom';
 
-const GadientPicker = () => {
+function GradientPicker() {
+  const [angle, setAngle] = useState(0);
+  const [color1, setColor1] = useState('#FFFFFF');
+  const [color2, setColor2] = useState('#000000');
+  const [percentage, setPercentage] = useState(50);
+  const [position, setPosition] = useState(0);
+  const [gradientType, setGradientType] = useState('linear');
+
+  const updateGradient = () => {
+    const angleValue = angle + 'deg';
+    const percentageValue = percentage + '%';
+    const positionValue = position + '%';
+    const gradient = gradientType === 'linear'
+      ? `linear-gradient(${angleValue}, ${color1} ${positionValue}, ${color2} 100%)`
+      : `radial-gradient(circle, ${color1} ${percentageValue}, ${color2} 100%)`;
+    return gradient;
+  };
+
+  const cssCode = `background: ${updateGradient()};`;
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(cssCode);
+    toast.success('Copied')
+  };
+
   return (
-    <div>GadientPicker</div>
-  )
+    <div className="w-full flex flex-col justify-center items-center mx-auto p-6 rounded-md font-bold">
+      <div className="lg:w-1/2 w-[100%] flex flex-col justify-center items-center font-normal mb-4">
+        <h1 className="font-bold lg:text-[4vw] text-[10vw] leading-[10vw] lg:leading-[4vw] text-center">
+          Gradient Picker
+        </h1>
+        <p className="lg:text-[1.4vw] text-[5vw] text-center w-full lg:w-[60%]">
+          A simple fast and lightweight Gradient picker for all your needs. Pick
+          your Gradient!
+        </p>
+      </div>
+        <div className="lg:w-1/2 w-full lg:h-[50vh] h-[30vh] rounded-xl flex justify-center items-center text-xl font-bold" style={{ background: updateGradient() }}></div>
+          <div className='lg:w-[30%] w-full flex flex-col'>
+          <div className="flex items-center justify-between mt-2 text-xl">
+        <label className="mr-2 ">Gradient Type:</label>
+        <select value={gradientType} onChange={e => setGradientType(e.target.value)} className="rounded-md border border-black p-2">
+          <option value="linear">Linear Gradient</option>
+          <option value="radial">Radial Gradient</option>
+        </select>
+      </div>
+      <div className="flex items-center justify-between mt-4 text-xl">
+        <label className="mr-2">Angle:</label>
+        <input type="range" min="0" max="360" value={angle} onChange={e => setAngle(e.target.value)} />
+      </div>
+      {gradientType === 'linear' && (
+        <div className="flex items-center justify-between mt-2 text-xl">
+          <label className="mr-2">Position:</label>
+          <input type="range" min="0" max="100" value={position} onChange={e => setPosition(e.target.value)} />
+        </div>
+      )}
+      {gradientType === 'radial' && (
+        <div className="flex items-center justify-between mt-2 text-xl">
+          <label className="mr-2">Percentage:</label>
+          <input type="range" min="0" max="100" value={percentage} onChange={e => setPercentage(e.target.value)} />
+        </div>
+      )}
+      <div className="flex items-center justify-between mt-2 text-xl">
+        <label className="mr-2">Color 1:</label>
+        <input type="color" value={color1} onChange={e => setColor1(e.target.value.toUpperCase())} className='lg:w-[3vw] lg:h-[3vw] h-[10vw] w-[10vw] rounded-xl'/>
+      </div>
+      <div className="flex items-center justify-between mt-2 text-xl">
+        <label className="mr-2">Color 2:</label>
+        <input type="color" value={color2} onChange={e => setColor2(e.target.value.toUpperCase())} className='lg:w-[3vw] lg:h-[3vw] h-[10vw] w-[10vw] rounded-xl'/>
+      </div>
+    
+    
+      <div className="mt-4">
+        <textarea value={cssCode} readOnly rows="4" className="w-full font-mono rounded-md  border border-black p-2"></textarea>
+      </div>
+      <div className="flex justify-between mt-4">
+        <Button className='bg-[#0611f2] text-[white] border-[#0611f2] uppercase hover:bg-white hover:text-[#0611f2] transition-all duration-300 font-bold ' onClick={copyToClipboard}>Copy Gradient</Button>
+      </div>
+          </div>
+          <h1 className='mt-4 font-normal'>Want a Color? Check out Our <Link to='/Color-picker' className="text-[#0611f2]">Color Picker!</Link></h1>
+          <Toaster richColors position="top-right" duration={3000} toastOptions={{classNames: {title: 'text-[#0611f2]'}}}/>
+    </div>
+  );
 }
 
-export default GadientPicker
+export default GradientPicker;
